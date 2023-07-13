ff27e5bd5b1f45b8d63171021fdf95433161a9a2
jc Komi <Assma.El.Komy@ibm.com>
2023-07-13 11:38:05 +0300

    new branch added

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+'''
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+driver.get("http://www.google.com/")
+print(driver.title)
+'''
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+driver.get("https://www.google.com/")
