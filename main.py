29bad0b62eaf82cd871442fd197b87bada76d88c
jc Komi <Assma.El.Komy@ibm.com>
2023-07-13 11:38:05 +0300

    new branch added

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+#  from webdriver_manager.chrome import ChromeDriverManager
+#  from selenium import webdriver
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# code from https://www.geeksforgeeks.org/python-script-to-open-a-web-browser/
+# first import the module
+import webbrowser
+# then make a url variable
+url = "https://magento.Softwaretestingboard.Com"
+# then call the default open method described above
+webbrowser.open(url)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+'''
+# my code worked but closes immediately 
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+executable_path = ChromeDriverManager().install()
+driver = webdriver.Chrome()
+driver.get("http://www.google.com/")
+print(driver.title)
+'''
+# code from ppt
+'''
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
+driver.get("https://www.google.com/")
+'''
